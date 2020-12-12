e29f75b65d3120acdab1d04031075430b280532d
redacteduk <hazel.savage@live.com>
2020-12-12 00:24:05 +0000

    day 9 part 1

--- day9-1.py
+++ day9-1.py
@@ -1,6 +1,16 @@
 
 # What is the first number that is not the sum of two of the five numbers immediately before it?
 
+
+def readInput(file):
+    seq = []
+    file = open(file, "r")
+    for line in file.readlines():
+        seq.append(int(line.rstrip()))
+    print(f'{len(seq)} lines read in.')
+    return seq
+
+
 # example has 5 preamble lines, then each number should be the sum of two of the preceeding five numbers
 exampleInput = [
     35,
@@ -26,14 +36,13 @@ exampleInput = [
 ]
 
 
-def grabData(seq):
+def grabData(seq, preambleAmount):
     line = 0
     count = 0
     data = []
     set = []
-    preambleAmount = 5
     #print(f'seq length: {len(seq)}')
-    while len(data) < (len(seq)-5):
+    while len(data) < (len(seq)-preambleAmount):
         num = seq[line]
         if count < preambleAmount:
             set.append(num)
@@ -42,18 +51,44 @@ def grabData(seq):
             #print(f'line: {line}')
         elif count == preambleAmount:
             sum = num
-            group = (set, sum)
+            group = [set, sum]
             # print(group)
             data.append(group)
             count = 0
-            line -= 4
+            line -= preambleAmount - 1
             #print(f'line: {line}')
             set = []
         else:
             return 'huh?'
+    print(f'{len(data)} preamble & number sets found.')
     return data
 
 
-data = grabData(exampleInput)
-for line in data:
-    print(line)
+def doMaths(data):
+    count = 0
+    checkedData = data
+    for line in checkedData:
+        for num1 in line[0]:
+            for num2 in line[0]:
+                if (num1 + num2 == line[1]) and (num1 != num2):
+                    if True not in line:
+                        line.append(True)
+                        count += 1
+                    # print(line)
+    print(f'{count} \'sum\' sets found.')
+    return checkedData
+
+
+def search(checkedData):
+    noSums = []
+    for line in checkedData:
+        if True not in line:
+            noSums.append(line)
+    return noSums
+
+
+seq = readInput('day9-input.txt')
+data = grabData(seq, 25)
+checkedData = doMaths(data)
+noSums = search(checkedData)
+print(f'The first \'no-sum\' number is {noSums[0][1]}.')
